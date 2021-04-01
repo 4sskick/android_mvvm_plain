e860a94d0c43d054e542786f2105a97f8405d21f
Septian Adi Wijaya <developadi@gmail.com>
2021-04-01 16:10:53 +0700

    setup adapter - model class

---
+++ app/src/main/java/com/niteroomcreation/mvvmvanila/model/Places.java
@@ -0,0 +1,35 @@
+package com.niteroomcreation.mvvmvanila.model;
+
+/**
+ * Created by Septian Adi Wijaya on 01/04/2021.
+ * please be sure to add credential if you use people's code
+ */
+public class Places {
+
+    private String imgUrl;
+    private String name;
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Places{" +
+                "imgUrl='" + imgUrl + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/niteroomcreation/mvvmvanila/view/adapter/MainAdapter.java
+++ app/src/main/java/com/niteroomcreation/mvvmvanila/view/adapter/MainAdapter.java
@@ -1,10 +1,19 @@
 package com.niteroomcreation.mvvmvanila.view.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.niteroomcreation.mvvmvanila.R;
+import com.niteroomcreation.mvvmvanila.model.Places;
+
+import java.util.List;
 
 /**
  * Created by Septian Adi Wijaya on 01/04/2021.
@@ -12,26 +21,49 @@ import androidx.recyclerview.widget.RecyclerView;
  */
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
 
+    private Context context;
+    private List<Places> places;
+
+    public MainAdapter(Context context, List<Places> places) {
+        this.context = context;
+        this.places = places;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.i_main, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.binds();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return places.size();
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public Places getItem(int pos) {
+        return places.get(pos);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView img;
+        private TextView txt;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            img = itemView.findViewById(R.id.item_img);
+            txt = itemView.findViewById(R.id.item_txt);
+        }
+
+        void binds() {
+            txt.setText(getItem(getAdapterPosition()).getName());
         }
     }
 }
